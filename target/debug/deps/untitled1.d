35f2a0a50083c3114c376b7bce5fd7ddf25e6e84
Helodie9977 <elodie9977@outlook.com>
2022-06-19 10:09:39 -0400

    Add a new project to Git

---
+++ target/debug/deps/untitled1.d
@@ -0,0 +1,5 @@
+C:\Users\E.Hernando\CLionProjects\untitled1\target\debug\deps\untitled1.exe: src\main.rs
+
+C:\Users\E.Hernando\CLionProjects\untitled1\target\debug\deps\untitled1.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/untitled1.d
@@ -0,0 +1,1 @@
+C:\Users\E.Hernando\CLionProjects\untitled1\target\debug\untitled1.exe: C:\Users\E.Hernando\CLionProjects\untitled1\src\main.rs
